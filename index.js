f3de1ec35d5adddfec368e3dc7ad9dd6c171d857
ikuromori <83898553+ikuromori@users.noreply.github.com>
2021-06-05 06:53:14 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var params = (new URL(document.location)).searchParams;
+
+$(function () {
+    $('form').submit(function () {
+        var height = $('textarea[name="height"]').val();
+        var weight = $('textarea[name="weight"]').val();
+        var msg = `${height}\n
+                　　${weight}`;
+        sendText(msg);
+        return false;
+    });
+});
