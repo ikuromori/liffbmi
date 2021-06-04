f3b02d84c235176f122c88a5715d14474f3af2bf
ikuromori <83898553+ikuromori@users.noreply.github.com>
2021-06-05 07:20:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ var params = (new URL(document.location)).searchParams;
 $(function () {
     $('form').submit(function () {
         var height = $('textarea[name="height"]').val();
-        var weight = $('textarea[name="weight"]').val();
-         var msg = 'あ';
-//         var msg = `${height}${weight}`;
+        var weight = $('textarea[name="weight"]').val();        
+        var msg = `${height}${weight}`;
         sendText(msg);
         return false;
     });
